b65e70948324a0734cb9bd8835d34fc715398537
Anjola Ogunmefun <you@example.com>
2021-06-14 17:33:26 +0100

    style: fix modal display

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import BaseButton from "./components/BaseComponents/BaseButton.vue";
 import UndrawFooter from './components/BaseComponents/UndrawFooter.vue'
 import UndrawHeader from './components/BaseComponents/UndrawHeader'
 import BaseSpinner from './components/BaseComponents/BaseSpinner.vue'
-
+import BaseDialog from './components/BaseComponents/BaseDialog.vue'
 
 const app = createApp(App);
 
@@ -17,5 +17,6 @@ app.component("base-button", BaseButton);
 app.component('undraw-footer', UndrawFooter)
 app.component('undraw-header', UndrawHeader)
 app.component('base-spinner', BaseSpinner)
+app.component('base-dialog', BaseDialog)
 
 app.mount("#app");
